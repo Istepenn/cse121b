5e170d557009b789c78ef39e839ef5c06d5cd817
Istepenn <suitonkyle@gmail.com>
2024-01-23 22:51:47 +0800

    adding

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,67 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+// Declare and instantiate a variable to hold your name
+const fullName = 'Stephen Kyle Suiton';
+
+// Declare and instantiate a variable to hold the current year
+const currentYear = new Date().getFullYear();
+
+// Declare and instantiate a variable to hold the file path and name of the image
+const profilePicture = 'w02-task/images/mypic.jpg';
+
+/* Step 3 - Element Variables */
+
+// Use document.getElementById() to get the name element
+const nameElement = document.getElementById('name');
+
+// Use document.getElementById() to get the food element
+const foodElement = document.getElementById('food');
+
+// Use document.querySelector() to get the year element
+const yearElement = document.querySelector('#year');
+
+// Use any viable method to get the profile image element
+const imageElement = document.getElementById('profile-image');
+
+/* Step 4 - Adding Content */
+
+// Assign nameElement's innerHTML property the fullName variable value
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+// Use textContent to set the value of yearElement to the currentYear variable value
+yearElement.textContent = currentYear;
+
+// Use setAttribute to set the src property of the imageElement to the profilePicture variable
+imageElement.setAttribute('src', profilePicture);
+
+// Use setAttribute to set the alt property of the imageElement
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+
+// Declare an array variable to hold your favorite foods
+const favoriteFoods = ['Pizza', 'Chocolate', 'Sushi'];
+
+// Declare and instantiate a variable to hold another single favorite food item
+const newFavoriteFood = 'Ice Cream';
+
+// Add the value stored in newFavoriteFood to your favoriteFoods array
+favoriteFoods.push(newFavoriteFood);
+
+// Remove the first element in the favoriteFoods array
+favoriteFoods.shift();
+
+// Remove the last element in the favoriteFoods array
+favoriteFoods.pop();
+
+// Update the content of the foodElement with the modified favoriteFoods array
+foodElement.innerHTML = favoriteFoods.join('<br>');
+
+
+
+
+
+
